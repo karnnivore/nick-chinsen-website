b2e9b6ecd71a00ad3afa5d52d36d8f2ea8defded
Nick Chinsen <nicholaschinsen@gmail.com>
2021-12-10 02:07:01 -0500

    Update intro subtext

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,9 +67,9 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <section className={styles.landingTextSection}>
           <div className={styles.sectionContainer}>
-            <h3>Hi, I&apos;m</h3>
+            <h3 className={styles.hi}>Hi, my name is</h3>
             <h1 className={styles.name}>Nick Chinsen.</h1>
-            <h2 className={styles.nameSubText}>I love to build web applications.</h2>
+            <h2 className={styles.nameSubText}>I love to build web apps.</h2>
             <p className={styles.landingIntroText}>
               I&apos;m a Toronto based full stack developer who specializes in creating 
               extraordinary digital experiences. Currently, I&apos;m a full stack developer,
