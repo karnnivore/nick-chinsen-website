5e718fdee7e0f0e6e40db8a7279967674579dea3
Nick Chinsen <nicholaschinsen@gmail.com>
2021-12-12 14:59:29 -0500

    Update about section, and add a headshot

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -1,31 +1,43 @@
 import React from 'react'
 import styles from '../../styles/home/About.module.css'
 import { SectionContainer } from '../layout/SectionContainer'
+import Image from 'next/image'
 
 export const About = () => {
+  const lbc = "https://lbcit.ca/"
+  const axcys = "http://www.axcysfinishing.ca/about.html"
+  const crueTV = "https://cruetv.com/home"
   return (
     <SectionContainer>
       <section className={styles.aboutSection} id="about-section">
-        <h1>About me</h1>
-        <p>
-          Hi! My name is Nicholas but I usually go by Nick, I love developing interesting
-          things for the web. My first foray into web development was my first website 
-          which I built in 2012 (basic html/css forever hidden from the world &#9786;) - 
-          That project sparked my initial obsession with web development and later coding 
-          which has continued till today. Since then, I&apos;ve gone to and finished school for
-          computer science and I haven&apos;t stopped tinkering, striving to learn something new 
-          every single day. 
-        </p>
-        <p>
-          I&apos;ve had the pleasure of working at <a>a web development agency</a>, 
-          <a>a small business</a>, <a>a local non-profit</a>, and I&apos;ve done freelance
-          projects throughout it all. Nowadays my main focus is building responsive & 
-          accessible full-stack applications and expereinces at <a>LBC IT Solutions</a> 
-          for a wide range of clients.
-        </p>
-        <p>
-          Here are some of the technologies I have been working with recently:
-        </p>
+        <div className={styles.textContainer}>
+          <h1>About me</h1>
+          <p>
+            Hi! My name is Nicholas but I usually go by Nick, I love developing interesting
+            things for the web. 
+          </p>  
+          <p>
+            My first foray into web development was my first website 
+            which I built in 2012 (basic html/css forever hidden from the world &#9786;) - 
+            That project sparked my initial obsession with web development and later coding 
+            which has continued till today. 
+          </p>
+          <p>
+            Since then, I&apos;ve gone to and finished school for
+            computer science and I haven&apos;t stopped tinkering, striving to learn something new 
+            every single day. 
+          </p>
+          <p>
+            I&apos;ve had the pleasure of working at <a href={lbc} target="_blank">a web development agency</a>, 
+            <a href={axcys} target="_blank"> a small business</a>, 
+            <a href={crueTV} target="_blank"> a local non-profit</a>, and I&apos;ve done freelance
+            projects throughout it all. Nowadays my main focus is building responsive & 
+            accessible full-stack applications and experiences at <a href={lbc} target="_blank">LBC IT Solutions </a> 
+            for a wide range of clients.
+          </p>
+          <p>
+            Here are some of the technologies I have been working with recently:
+          </p>
           <ul className={styles.listAboutMe}>
             <li>TypeScript</li>
             <li>JavaScript ES6+</li>
@@ -34,7 +46,17 @@ export const About = () => {
             <li>WordPress</li>
             <li>Python</li>
           </ul>
-       
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.headshotImage}
+            src="/images/nick.jpg"
+            alt="Picture of me Nick"
+            width={300}
+            height={300}
+            layout="fixed"
+          />
+        </div>
       </section>
     </SectionContainer>
   )
