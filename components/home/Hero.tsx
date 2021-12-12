a608a718f88e6db9f0135da48f9e58f6ce07519f
Nick Chinsen <nicholaschinsen@gmail.com>
2021-12-12 15:00:28 -0500

    Update hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -9,9 +9,9 @@ export const Hero = () => {
           <h1 className={styles.name}>Nick Chinsen</h1>
           <h2 className={styles.nameSubText}>Innovation never stops I don&apos;t either</h2>
           <p className={styles.landingIntroText}>
-            I&apos;m a Toronto based full stack developer who specializes in creating 
-            extraordinary digital experiences. I&apos;m currently developing human-centered 
-            products at <a>LBC IT Solutions</a>.
+            I&apos;m a full stack developer who specializes in creating 
+            extraordinary digital experiences. I&apos;m currently developing accessible,
+            modern products at <a>LBC IT Solutions</a>.
           </p>
           <button>
             <a href="#about-section">
