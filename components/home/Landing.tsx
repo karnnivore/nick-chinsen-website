6e699946a2825585dec4673a2a724671d5e967cc
Nick Chinsen <nicholaschinsen@gmail.com>
2021-12-12 19:26:56 -0500

    Create landing scene

--- components/home/Landing.tsx
+++ components/home/Landing.tsx
@@ -5,25 +5,31 @@ import Image from 'next/image'
 export const Landing = () => {
   return (
     <SectionContainer>
-      <div className={styles.imageContainer}>
-        <img
-        src="/images/N1.svg"
-        className={styles.n1}
-        height={500}
-        width={500}
-        />
-        <img
-        src="/images/N2.svg"
-        className={styles.n2}
-        height={500}
-        width={500}
-        />
-        <img
-        src="/images/N3.svg"
-        className={styles.n3}
-        height={500}
-        width={500}
-        />
+      <div className={styles.landingContainer}> 
+        <div className={styles.imageContainer}>
+          <img
+          src="/images/N1.svg"
+          className={styles.n1}
+          height={500}
+          width={500}
+          />
+          <img
+          src="/images/N2.svg"
+          className={styles.n2}
+          height={500}
+          width={500}
+          />
+          <img
+          src="/images/N3.svg"
+          className={styles.n3}
+          height={500}
+          width={500}
+          />
+        </div>
+        <a href="#hero-section" className={styles.hiButton}>
+          <span>H I</span>
+          <img src="/images/chevron-down-solid.svg" height={35} width={35}/>
+        </a>
       </div>
     </SectionContainer>
   )
